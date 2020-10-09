101236e35b94ab2f758cc2abeb798f017dc0e799
Siquoyia <70215872+Siquoyia@users.noreply.github.com>
2020-10-09 16:37:25 -0700

    uno.py update

--- uno.py
+++ uno.py
@@ -14,6 +14,10 @@ class Card:
 class Player:
     def __init__(self, name):
         self.name = name 
+        self.hand = []
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Deck:
@@ -24,7 +28,34 @@ class Deck:
                 card = (color, number)
                 self.cards.append(card)
         
-
+    def shuffle(self):
+        def shuffle(self):
+         shuffle_deck:list = []    
+         deck_to_shuffle = self.cards.copy()
+        while len(deck_to_shuffle) > 0:
+            # pull random card from unshuffled deck
+            card_to_move = deck_to_shuffle [randint(0, len(deck_to_shuffle) -1)]
+            # put it in random deck
+            deck_to_shuffle.remove(card_to_move)
+            # now remove it from original deck
+            shuffle_deck.append(card_to_move)
+        return shuffle_deck
 
 class Game:
-    pass
+    def __init___(self):
+        self.deck = Deck(NUMBERS, COLORS).shuffle()
+        self.player1 = Player("Joe")
+        self.player2 = Player("Mary")
+        for card in self.deck:
+            print(card)
+        print(self.player1, self.player2)
+
+    def deal_cards(self):
+        for i in range(7):
+          self.player1.hand.append(self.deck.pop())
+          self.player2.hand.append(self.deck.pop())
+        print(len(self.player1.hand), len(self.player2.hand))
+
+game = Game()
+deck = game.deck 
+game.deal_cards()
